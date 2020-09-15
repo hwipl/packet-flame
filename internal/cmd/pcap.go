039b95af13443637fd00a52f5b82c6562c982977
hwipl <33433250+hwipl@users.noreply.github.com>
2020-09-15 13:00:31 +0200

    print layers
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/pcap.go
+++ internal/cmd/pcap.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/google/gopacket"
 	"github.com/hwipl/packet-go/pkg/pcap"
 )
@@ -8,6 +10,15 @@ import (
 type handler struct{}
 
 func (h *handler) HandlePacket(packet gopacket.Packet) {
+	layers := ""
+	for _, l := range packet.Layers() {
+		if layers != "" {
+			layers += ","
+		}
+		layers += l.LayerType().String()
+
+	}
+	fmt.Println(layers)
 }
 
 // listen captures packets on the network interface or reads them from a pcap
